b55dc8b555c37bcd962e9f1abed00d91e307951e
TranThunder <trunghieu25102004@gmail.com>
2024-11-25 22:42:58 +0700

    dissolve

---
+++ Assets/AxisInterpolation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisInterpolation : MonoBehaviour
+{
+    public Vector3 targetRotation;
+    public float transitionValue;
+
+    void Update()
+    {
+        Quaternion targetRotationA = Quaternion.Euler(targetRotation);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationA, transitionValue);
+    }
+}
